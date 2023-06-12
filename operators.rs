bbe363253cdb69174d40608843cb27eb2ad9684f
s1834 <s18324@protonmail.com>
2023-06-13 03:40:48 +0530

    :sparkles: Comparison Operators

--- operators.rs
+++ operators.rs
@@ -10,9 +10,26 @@ fn main() {
     let remainder = a % b; 
     let negation = -a;
     println!("Sum: {}", sum); 
-    println!("Difference: {}", difference); println!("Product: {}", product); 
+    println!("Difference: {}", difference);
+    println!("Product: {}", product); 
     println!("Quotient: {}", quotient); 
     println!("Remainder: {}", remainder); 
     println!("Negation: {}", negation);
     println!("\n");
+
+    // COMPARISON OPERATORS
+    println!("COMPARISON OPERATORS");
+    let equals = a == b;
+    let not_equals = a != b;
+    let greater_than = a > b;
+    let greater_than_or_equal = a >= b; let less_than = a < b;
+    let less_than_or_equal = a <= b;
+    println!("Equals: {}", equals);
+    println!("Not Equals: {}", not_equals);
+    println!("Greater Than: {}", greater_than);
+    println!("Greater Than or Equal: {}", greater_than_or_equal); 
+    println!("Less Than: {}", less_than);
+    println!("Less Than or Equal: {}", less_than_or_equal);
+    println!("\n");
+
     }
