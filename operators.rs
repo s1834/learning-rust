a8c23f48e6788f58c6d9ebff1358114618ef3521
s1834 <s18324@protonmail.com>
2023-06-13 03:38:15 +0530

    :sparkles: Arithemetic Operators

---
+++ operators.rs
@@ -0,0 +1,18 @@
+fn main() { 
+    // ARITHMETIC OPERATORS
+    println!("ARITHMETIC OPERATORS");
+    let a = 5; 
+    let b = 3;
+    let sum = a + b;
+    let difference = a - b; 
+    let product = a * b;
+    let quotient = a / b; 
+    let remainder = a % b; 
+    let negation = -a;
+    println!("Sum: {}", sum); 
+    println!("Difference: {}", difference); println!("Product: {}", product); 
+    println!("Quotient: {}", quotient); 
+    println!("Remainder: {}", remainder); 
+    println!("Negation: {}", negation);
+    println!("\n");
+    }
