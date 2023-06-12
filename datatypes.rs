84b860619f5c756d98d8bf34f33e1caa74521795
s1834 <s18324@protonmail.com>
2023-06-13 02:48:25 +0530

    :sparkles: Integers

---
+++ datatypes.rs
@@ -0,0 +1,13 @@
+fn main() {
+    // INTEGER
+    let result = 10; //i32 by default
+    let age: u32 = 20;
+    let sum: i32 = 5 - 15;
+    let mark: isize = 10;
+    let count: usize = 30;
+
+    println!("Result value: {}", result);
+    println!("Sum is {} and age is {}", sum, age);
+    println!("Mark is {} and count is {}", mark, count);
+
+}
