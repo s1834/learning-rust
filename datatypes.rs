36f40a43bf4fbbec093f455851a59413b0a2f61f
s1834 <s18324@protonmail.com>
2023-06-13 03:04:40 +0530

    :sparkles: Characters

--- datatypes.rs
+++ datatypes.rs
@@ -6,7 +6,6 @@ fn main() {
     let sum: i32 = 5 - 15;
     let mark: isize = 10;
     let count: usize = 30;
-
     println!("Result value: {}", result);
     println!("Sum is {} and age is {}", sum, age);
     println!("Mark is {} and count is {}", mark, count);
@@ -17,7 +16,6 @@ fn main() {
     let result1 = 10.00; // f64 by default
     let interest: f32 = 8.35;
     let cost: f64 = 15000.600; // double precision
-
     println!("Result value is {}", result1);
     println!("interest is {}", interest);
     println!("cost is {}", cost);
@@ -28,5 +26,15 @@ fn main() {
     let is_fun: bool = true;
     println!("Is Rust Programming Fun? {}", is_fun);
     println!("\n");
-    
+
+    // CHARACTERS
+    println!("CHARACTERS");
+    let special_character = '@'; // default
+    let alphabet: char = 'A';
+    let emoji: char = '😁';
+    println!("special character is {}", special_character);
+    println!("alphabet is {}", alphabet);
+    println!("emoji is {}", emoji);
+    println!("\n");
+
 }
