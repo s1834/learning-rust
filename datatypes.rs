d91d4bc4aaa6974a69dd2ea737d6d5f6c2e30e24
s1834 <s18324@protonmail.com>
2023-06-13 02:54:20 +0530

    :sparkles: Floating-Point

--- datatypes.rs
+++ datatypes.rs
@@ -1,5 +1,6 @@
 fn main() {
     // INTEGER
+    println!("INTEGERS");
     let result = 10; //i32 by default
     let age: u32 = 20;
     let sum: i32 = 5 - 15;
@@ -9,5 +10,16 @@ fn main() {
     println!("Result value: {}", result);
     println!("Sum is {} and age is {}", sum, age);
     println!("Mark is {} and count is {}", mark, count);
+    println!("\n");
 
+    // FLOATING-POINT
+    println!("FLOATING-POINT");
+    let result1 = 10.00; // f64 by default
+    let interest: f32 = 8.35;
+    let cost: f64 = 15000.600; // double precision
+
+    println!("Result value is {}", result1);
+    println!("interest is {}", interest);
+    println!("cost is {}", cost);
+    println!("\n");
 }
