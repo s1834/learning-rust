db28da8287d84a33db7d0b765dd87d6c38cb755a
s1834 <s18324@protonmail.com>
2023-06-16 03:33:49 +0530

    :sparkles: Call by Refrence

--- functions.rs
+++ functions.rs
@@ -1,10 +1,17 @@
+// Simple Function
 fn square(num: i32) -> i32 {
     let result = num * num;
     result
 }
 
+// Call by Value (By Default)
 fn add_one(num: i32) -> i32 {
     num + 1 // Increment the parameter by 1
+}
+
+// Call by Refrence
+fn add_one_by_ref(num: &mut i32) {
+    *num += 1 // Increment the value pointed by the refrence
 }
 
 fn main() {
@@ -14,4 +21,9 @@ fn main() {
 
     let result = add_one(number);
     println!("Original Value: {}, Result: {}\n", number, result);
+
+    let mut ref_number = 5;
+    add_one_by_ref(&mut ref_number);
+    println!("Modified Value: {}\n", ref_number);
+
 }
