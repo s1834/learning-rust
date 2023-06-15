0dd7013a7dc59170a0e2bd50b5be978bfa6305ea
s1834 <s18324@protonmail.com>
2023-06-16 04:01:58 +0530

    :sparkles: Returning Tuples from Functions

--- tuples.rs
+++ tuples.rs
@@ -1,7 +1,31 @@
+fn get_person1() -> (String, u32, bool) {
+    let name = String::from("Alice");
+    let age = 30;
+    let is_active = false;
+    (name, age, is_active)
+}
+
 fn main() {
+    // Simple Tuple Example
     let person = ("John", 25, true); 
     println!("{:?}", person);
+
+    // Tuple access
     println!("Name: {}", person.0);
     println!("Age: {}", person.1);
     
+    // Tuple Deconstructing
+    let (name, age, is_active) = person; 
+    println!("Name: {}", name); 
+    println!("Age: {}", age); 
+    println!("Is Active: {}", is_active);
+
+    // Returning Tuples from Functions
+    let person1 = get_person1();
+    println!("Name: {}", person1.0);
+    println!("Age: {}", person1.1);
+    println!("Is Active: {}", person1.2);
+
+
+
 }
