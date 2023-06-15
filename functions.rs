a0399daaa4980cde101068db6a1abd2f11c1f40b
s1834 <s18324@protonmail.com>
2023-06-16 03:28:10 +0530

    :sparkles: Call By Value

--- functions.rs
+++ functions.rs
@@ -3,8 +3,15 @@ fn square(num: i32) -> i32 {
     result
 }
 
+fn add_one(num: i32) -> i32 {
+    num + 1 // Increment the parameter by 1
+}
+
 fn main() {
     let number = 5;
     let square_result = square(number);
     println!("Square of {} is {}\n", number, square_result);
+
+    let result = add_one(number);
+    println!("Original Value: {}, Result: {}\n", number, result);
 }
