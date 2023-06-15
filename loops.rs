e7456453d37e4b32cea908689d75d6ea98a830dc
s1834 <s18324@protonmail.com>
2023-06-16 03:47:15 +0530

    :sparkles: 'loop' loop

--- loops.rs
+++ loops.rs
@@ -1,3 +1,15 @@
 fn main() {
+    let mut count = 0;
     
+    // 'loop' loop
+    println!("'loop' loop");
+    loop {
+        println!("Current count: {}", count); 
+        count += 1;
+        if count == 5 {
+            break; 
+        }
+    }
+    println!("\n");
+
 }
