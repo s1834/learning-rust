76ce60e3426f599c149cbdad5cb9ac3efd75316a
s1834 <s18324@protonmail.com>
2023-06-16 03:42:13 +0530

    :sparkles: Match expression

--- conditional-statements.rs
+++ conditional-statements.rs
@@ -8,4 +8,11 @@ fn main() {
         println!("Number is zero");
     }
 
+    let fruit = "apple"; 
+    match fruit {
+        "apple" => println!("It's an apple"), 
+        "banana" => println!("It's a banana"), 
+        _ => println!("It's something else")
+    }
+
 }
