3fbdc4be6d946dea778d7c1d9ffecce4f87cab95
s1834 <s18324@protonmail.com>
2023-06-17 01:10:12 +0530

    :sparkles: Structures Done

--- structure.rs
+++ structure.rs
@@ -1,3 +1,22 @@
+// Structure Defination
+#[derive(Debug)]
+struct Person {
+    name: String,
+    age: u32,
+    is_active: bool,
+}
+
 fn main () {
+    // Structure Instanciation
+    let person = Person {
+        name: String::from("John"), 
+        age: 25,
+        is_active: true,
+    };
+    println!("{:?}",person);
+
+    // Strcture Field Access
+    println!("Name: {}", person.name); 
+    println!("Age: {}", person.age);
     
 }
