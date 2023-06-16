fbc97b548d4730f4babb59f632fb488ba4bcbc13
s1834 <s18324@protonmail.com>
2023-06-17 00:35:27 +0530

    :sparkles: Array Access

--- array.rs
+++ array.rs
@@ -3,4 +3,8 @@ fn main() {
     let numbers: [i32; 4] = [10, 20, 30, 40];
     println!("{:?}", numbers);
 
+    // Array Access
+    println!("First element: {}", numbers[0]); 
+    println!("Second element: {}", numbers[1]);
+
 }
