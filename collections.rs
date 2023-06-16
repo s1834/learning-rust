c26d3d10a10aba4a7cccb98aed90e4ab0814ba67
s1834 <s18324@protonmail.com>
2023-06-17 02:55:54 +0530

    :sparkles: Vectors

--- collections.rs
+++ collections.rs
@@ -1,3 +1,21 @@
 fn main() {
+    // VECTORS
+    println!("VECTORS");
+    let mut numbers: Vec<i32> = Vec::new(); // Create an empty vector 
+    numbers.push(1); // Add an element to the vector 
+    numbers.push(2);
+    numbers.push(3);
+
+    println!("{:?}", numbers); // Print the vector
+    
+    // Access elements of the vector
+    let first_element = numbers[0]; 
+    println!("First element: {}", first_element);
+    
+    // Iterate over the vector
+    for number in &numbers { 
+        println!("{}", number);
+    } 
+    println!("\n");
     
 }
