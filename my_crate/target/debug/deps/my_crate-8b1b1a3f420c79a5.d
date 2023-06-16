3d2c7b33138fd6d516ad3772947e12fc816a8e68
s1834 <s18324@protonmail.com>
2023-06-17 05:17:10 +0530

    :tada: Learning Crates

---
+++ my_crate/target/debug/deps/my_crate-8b1b1a3f420c79a5.d
@@ -0,0 +1,5 @@
+/Users/s1834/Desktop/Learning Rust/my_crate/target/debug/deps/my_crate-8b1b1a3f420c79a5: src/main.rs
+
+/Users/s1834/Desktop/Learning Rust/my_crate/target/debug/deps/my_crate-8b1b1a3f420c79a5.d: src/main.rs
+
+src/main.rs:
---
+++ my_crate/target/debug/my_crate.d
@@ -0,0 +1,1 @@
+/Users/s1834/Desktop/Learning\ Rust/my_crate/target/debug/my_crate: /Users/s1834/Desktop/Learning\ Rust/my_crate/src/main.rs
